f2f3ed2c7b0a5f0ebd0f0d3afa83c0fb86c5b454
Klezya <benjamin.nunez.beat@gmail.com>
2023-11-02 10:24:21 -0300

    a

---
+++ Arboles binarios/funciones.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct nodo {
+    int info;
+    struct nodo *left;
+    struct nodo *right;
+} tNodo;
+
+typedef tNodo *AB;
+
+AB crearNodo(){
+
+}
